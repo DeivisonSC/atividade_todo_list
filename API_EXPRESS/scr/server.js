58a70ce61092ea38f06980f3dcf1d74fe80532d1
Deivison Santana Calazans <deivisonsantana189@gmail.com>
2026-04-01 16:30:03 -0300

    feat: estruturando servidor Express e sistema de autenticação

--- API_EXPRESS/scr/server.js
+++ API_EXPRESS/scr/server.js
@@ -2,17 +2,24 @@ require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
-const logRoutes = require('./routes/logRoutes');
+
+const usuarioRoutes = require('./routes/usuarioRoutes');
+//const tarefaRoutes = require('./routes/tarefaRoutes');
+
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/api', logRoutes);
+app.use('/api/usuarios', usuarioRoutes);
 
-const PORT = process.env.PORT;
+app.get('/', (req, res) => {
+    res.json({ mensagem: 'API de Tarefas rodando com sucesso! '});
+});
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Serviço rodando na porta ${PORT}`);
+    console.log(`Serviço de Tarefas rodando na porta ${PORT}`);
 });
